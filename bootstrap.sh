c8316099ea914140dab4e5578924907877e4201f
Fredrik <fvaagaard@gmail.com>
2025-11-21 13:49:56 +0100

    Install tmux plugins as part of bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,30 @@
 #/usr/bin/bash
 
-# Symlink configs
+# Stow dotfiles
+echo "Stowing dotfiles..."
 stow alacritty
 stow zsh
 stow nvim
 stow tmux
 
-# Symlink scripts and binaries
+# Stow scripts and binaries
+echo "Stowing scripts and binaries..."
 stow -t ~ bin
+
+set -e
+
+# Install TPM if not already installed
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+if [ ! -d "$TPM_DIR" ]; then
+    echo "Installing TPM..."
+    git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+else
+    echo "TPM already installed."
+fi
+
+# Install tmux plugins via TPM
+echo "Installing tmux plugins..."
+"$TPM_DIR/bin/install_plugins"
+
+echo "Tmux plugins installed."
